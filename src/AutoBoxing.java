c65a7d13414a18d2cc1800fa4bd1c5ef6ead31b4
zaixinlu <zaixinlu@gmail.com>
2019-01-06 14:03:13 -0800

    add vari args

--- src/AutoBoxing.java
+++ src/AutoBoxing.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 import static java.lang.System.out;
 
 public class AutoBoxing {
@@ -13,6 +16,20 @@ public class AutoBoxing {
         out.println(a + b);
         out.println(flag + " " + isBug);
 
+        List<String> hello = Arrays.asList("good", "nice", "A*");
+
+        for (String string : hello)
+            out.println(string);
+
+        print("good", "nice", "A*");
+
+        print(new String[] {"good", "nice", "A*"});
+
+    }
+
+    public static void print(String... strings) {
+        for (String string : strings)
+            out.println(string);
     }
 
 }
